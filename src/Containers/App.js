cbe8d7acf16cfe8133bb8d9230f42b84bcede9f8
baldm0mma <jev.forsberg@gmail.com>
2019-07-31 10:45:26 -0600

    Successful donations fetch and addition to store

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { getAllAnimals } from '../apiCalls';
-import { setAllAnimals, setErrorMessage } from '../Actions';
+import { getAllAnimals, getDonations } from '../apiCalls';
+import { setAllAnimals, setErrorMessage, setDonationData } from '../Actions';
 import { Card } from './Card';
 import './App.css';
 
@@ -13,25 +13,27 @@ export class App extends Component {
     } catch (error) {
       await this.props.onErroringOut(error.message);
     }
+    try {
+      const response = await getDonations();
+      await this.props.onFetchingDonations(response);
+    } catch (error) {
+      await this.props.onErroringOut(error.message);
+    }
   };
 
   populateCards = () => {
     return this.props.animals.map(animal => {
-      return <Card animal={animal} />;
+      return <Card animal={animal} key={animal.id} id={animal.id} />;
     });
   };
 
   render = () => {
     return (
       <main>
-        <header>
-
-        </header>
-        <section className='animals-container'>
-          {this.populateCards()}
-        </section>
+        <header></header>
+        <section className='animals-container'>{this.populateCards()}</section>
       </main>
-    )
+    );
   };
 }
 
@@ -41,7 +43,8 @@ export const mapStateToProps = ({ animals }) => ({
 
 export const mapDispatchToProps = dispatch => ({
   onFetchingAllAnimals: allAnimals => dispatch(setAllAnimals(allAnimals)),
-  onErroringOut: errorMessage => dispatch(setErrorMessage(errorMessage))
+  onErroringOut: errorMessage => dispatch(setErrorMessage(errorMessage)),
+  onFetchingDonations: donations => dispatch(setDonationData(donations))
 });
 
 export default connect(
--- src/apiCalls/index.js
+++ src/apiCalls/index.js
@@ -7,3 +7,13 @@ export const getAllAnimals = async () => {
     throw Error('Cannot fetch animals at this time');
   }
 };
+
+export const getDonations = async () => {
+  try {
+    const response = await fetch('http://localhost:3001/api/v1/donations');
+    const parsed = await response.json();
+    return parsed;
+  } catch (error) {
+    throw Error('Cannot fetch donations at this time');
+  }
+};
