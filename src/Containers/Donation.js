c045cb816459c8a845c6d631e5fc12b8b9da6f61
baldm0mma <jev.forsberg@gmail.com>
2019-07-31 12:19:24 -0600

    Add dollar sign to donations display

--- src/Containers/Donation.js
+++ src/Containers/Donation.js
@@ -5,7 +5,7 @@ export const Donation = ({ donation }) => {
   return (
     <article className='donation_card'>
       <h2>Name: {donation.name}</h2>
-      <p>Donation: {donation.donation}</p>
+      <p>Donation: ${donation.donation}</p>
     </article>
   );
 };
